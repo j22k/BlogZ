67b4e64e9461218e67448980b547cd46c926d08d
Junaid <junaidkaliyadan@gmail.com>
2023-08-06 21:52:55 +0530

    Created Sign in  and Sign up

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import SigInSignUp from './components/SignIn-SignUp/SignIn_SignUp'
+import React from 'react';
+function App() {
+  return (
+    <SigInSignUp />
+  );
+}
+
+export default App;
