13373a9dc6ce18af04cbef42abf02fd5677b07f1
Junaid <junaidkaliyadan@gmail.com>
2023-08-14 21:25:42 +0530

    Sign up routes in server

---
+++ server/helpers/userHelpers.js
@@ -0,0 +1,41 @@
+const { response } = require('express');
+const collections = require('../config/collections');
+const { getDatabase } = require('../config/connection');
+const db = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    dosignup: async (userData) => {
+        var status = false;
+        if (userData.password != userData.confirm_password){
+            return {status : false ,message : "password not match"};
+        }
+        try {
+          const db = getDatabase();
+
+          const salt = await bcrypt.genSalt(10);
+          const hashpass = await bcrypt.hash(userData.password, salt);
+          userData.password = hashpass;
+    
+          const existingUser = await db.collection(collections.USER).findOne({email : userData.email});
+          if (existingUser) {
+            return {status : false ,message : "User already exist"};
+          }
+
+          
+          const response = await db.collection(collections.USER).insertOne(userData);
+
+          if (response.acknowledged) {
+            console.log('New account for user created successfully');
+            status = true;
+            return true;
+          } else {
+            console.log("Couldn't create account");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return {status : false , err}
+        }
+      },
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,36 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { getDatabase } = require('../config/connection');
+const userHelpers = require('../helpers/userHelpers');
 
 router.post('/signup', async (req, res) => {
   console.log(req.body);
-  console.log('test -1');
-  console.log('test 0');
-  if (req.body.password != req.body.confirmpassword) {
-    console.log('not');
-    return res.status(400).json({ message: 'Password and confirm password do not match' });
-  }
-    try {
-      console.log('test 1');
-      const db = getDatabase();
-      const usersCollection = db.collection('users');
-      console.log('test 2');
-      // Check if the user with the same email already exists
-      const existingUser = await usersCollection.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'User with this email already exists' });
-      }
-      console.log('test 3');
-      // Insert the new user into the database
-      await usersCollection.insertOne({ username, email, password });
 
-      res.status(201).json({ message: 'Registration successful' });
-      console.log('test 4');
-    } catch (error) {
-      console.error('Error registering user:', error);
-      res.status(500).json({ message: 'Internal server error' });
-      console.log('test 5');
-    }
+  result = await userHelpers.dosignup(req.body);
+  console.log(result);
+  if(result.status){
+    res.sendStatus(200).json("registerd succesfully");
+  }
+  else{
+    console.log(result);
+    res.sendStatus(400).json(result.message);
+  }
   
 });
 
